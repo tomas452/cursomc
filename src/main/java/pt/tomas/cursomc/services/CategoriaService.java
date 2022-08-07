e1c642d2839313dcce0f10379712fbd611c31cd8
Tomás Gonçalves <tomasricardo1975@outlook.pt>
2022-08-07 14:55:47 +0100

    Ajuste no endpoint /categorias/{id}

--- src/main/java/pt/tomas/cursomc/services/CategoriaService.java
+++ src/main/java/pt/tomas/cursomc/services/CategoriaService.java
@@ -14,8 +14,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+	public Categoria buscar(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new pt.tomas.cursomc.services.exceptions.ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+		}
 }
