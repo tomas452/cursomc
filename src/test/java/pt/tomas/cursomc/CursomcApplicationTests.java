7f5188eb9cc0821aca37e847b15acd8775d9e93d
Tomás Gonçalves <tomasricardo1975@outlook.pt>
2022-08-07 07:46:17 +0100

    Criação do Projeto

---
+++ src/test/java/pt/tomas/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.tomas.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
