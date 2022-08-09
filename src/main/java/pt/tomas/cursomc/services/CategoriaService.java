affad1b7668a99499159c573108439a6565c623f
Tomás Gonçalves <tomasricardo1975@outlook.pt>
2022-08-09 01:43:38 +0100

    Inserindo uma nova categoria com POST

--- src/main/java/pt/tomas/cursomc/services/CategoriaService.java
+++ src/main/java/pt/tomas/cursomc/services/CategoriaService.java
@@ -14,9 +14,14 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) { 
-		 Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElseThrow(() -> new pt.tomas.cursomc.services.exceptions.ObjectNotFoundException( 
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
-		}
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new pt.tomas.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);//o objecto novo a ser inserido tem que ter o id nulo
+		return repo.save(obj);
+	}
 }
