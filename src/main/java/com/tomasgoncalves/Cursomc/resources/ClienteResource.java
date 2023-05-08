2e3f2d0e57acc909876cef192d66f444cb041281
Tomás Gonçalves <tomasricardo1975@outlook.pt>
2023-05-08 12:20:47 +0100

    Inserindo um novo Cliente com POST

--- src/main/java/com/tomasgoncalves/Cursomc/resources/ClienteResource.java
+++ src/main/java/com/tomasgoncalves/Cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.tomasgoncalves.Cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tomasgoncalves.Cursomc.domain.Cliente;
 import com.tomasgoncalves.Cursomc.dto.ClienteDTO;
+import com.tomasgoncalves.Cursomc.dto.ClienteNewDTO;
 import com.tomasgoncalves.Cursomc.services.ClienteService;
 
 import jakarta.validation.Valid;
@@ -31,6 +34,13 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/tomasgoncalves/Cursomc/services/ClienteService.java
+++ src/main/java/com/tomasgoncalves/Cursomc/services/ClienteService.java
@@ -10,11 +10,18 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.tomasgoncalves.Cursomc.domain.Cidade;
 import com.tomasgoncalves.Cursomc.domain.Cliente;
+import com.tomasgoncalves.Cursomc.domain.Endereco;
+import com.tomasgoncalves.Cursomc.domain.enums.TipoCliente;
 import com.tomasgoncalves.Cursomc.dto.ClienteDTO;
+import com.tomasgoncalves.Cursomc.dto.ClienteNewDTO;
 import com.tomasgoncalves.Cursomc.repositories.ClienteRepository;
+import com.tomasgoncalves.Cursomc.repositories.EnderecoRepository;
 import com.tomasgoncalves.Cursomc.services.exceptions.DataIntegrityException;
 import com.tomasgoncalves.Cursomc.services.exceptions.ObjectNotFoundException;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ClienteService {
@@ -22,10 +29,21 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -54,6 +72,24 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipoCliente()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
